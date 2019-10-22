8145e96f17e2026dfad086b121f819d9e7ad1eee
Artur Y <artyorsh@users.noreply.github.com>
2019-10-22 14:44:25 +0300

    feat(auth): add reset password and sign-up back buttons

--- src/scenes/auth/reset-password.component.tsx
+++ src/scenes/auth/reset-password.component.tsx
@@ -4,23 +4,30 @@ import {
   StyleSheet,
 } from 'react-native';
 import {
-  Formik,
-  FormikProps,
-} from 'formik';
+  EdgeInsets,
+  useSafeArea,
+} from 'react-native-safe-area-context';
 import {
   Button,
   Layout,
   LayoutElement,
 } from 'react-native-ui-kitten';
+import {
+  Formik,
+  FormikProps,
+} from 'formik';
 import { FormInput } from '@app-components/form-input.component';
 import {
   ResetPasswordData,
   ResetPasswordSchema,
 } from '@app-data/reset-password.model';
 import { AppRoute } from '@app-navigation/app-routes';
+import { Toolbar } from "@app-components/toolbar.component";
 
 // FIXME(REACT-NAVIGATION-5): props type definitions? (used in `auth.navigator.tsx`)
 export const ResetPasswordScreen = (props): LayoutElement => {
+
+  const insets: EdgeInsets = useSafeArea();
 
   const onFormSubmit = (values: ResetPasswordData): void => {
     navigateSignIn();
@@ -49,9 +56,13 @@ export const ResetPasswordScreen = (props): LayoutElement => {
   return (
     <React.Fragment>
       <ImageBackground
-        style={styles.appBar}
-        source={require('../../assets/image-note-background-1.jpg')}
-      />
+        style={[styles.appBar, { paddingTop: insets.top }]}
+        source={require('../../assets/image-note-background-1.jpg')}>
+        <Toolbar
+          appearance='control'
+          onBackPress={props.navigation.goBack}
+        />
+      </ImageBackground>
       <Layout style={styles.formContainer}>
         <Formik
           initialValues={ResetPasswordData.empty()}
