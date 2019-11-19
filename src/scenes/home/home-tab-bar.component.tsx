c57c2b511d28bbc7a2e752ec4a1b10a129ac3421
Artur Yorsh <artyorsh.me@gmail.com>
2019-11-19 12:30:50 +0300

    update dependencies

--- src/scenes/home/home-tab-bar.component.tsx
+++ src/scenes/home/home-tab-bar.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export const TodoScreen = (props): React.ReactElement => {
+export const HomeTabBar = (props): React.ReactElement => {
 
   return (
     <View/>
